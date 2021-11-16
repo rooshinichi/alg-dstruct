04cd318a2a985b835e65b9fce061875a61450f77
rooshinichi <60985778+rooshinichi@users.noreply.github.com>
2021-11-16 13:57:31 +0300

    Update main.c

--- 4_yuan.g_A25/main.c
+++ 4_yuan.g_A25/main.c
@@ -1,81 +1,46 @@
-﻿#include "A25.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include"A25.h"
+//----------------------------------------------//
 
-//-----------------------------------------------------------------------------//
-
-int main()
+void main() 
 {
-	FILE* input = fopen("TEXT.txt", "r");   //读取文件，返回文件的指针
-	if (NULL == input)
-	{
-		perror("/nerror NO_001 ");
-		return error;
-	}
-
-	char* string = malloc(sizeof(char) * 3 * NAME_LENGTH_MAX); //创建指针来存放从文件里读取的数据
-	if (NULL == string)
-	{
-		perror("/nerror NO_002 ");
-		return error;
-	}
-
-	node_t* head = NULL;   //创建空链表预备存储名字数据
-	while (!feof(input))   //读取文件是否到结尾
-	{
-		fgets(string, 3 * NAME_LENGTH_MAX, input);   //把数据先放到指针里
-
-		node_t* get_new_name = (node_t*)malloc(sizeof(node_t));  //存储一个名字然后再连接到链表上
-
-		if (NULL == get_new_name)
-		{
-			perror("/nerror NO_003 ");
-			return error;
-		}
-
-		int key = write_string_to_node_t(string, get_new_name);
-
-		if (!key)
-		{
-			perror("/nerror NO_004 ");
-			return error;
-		}  //把名字从string读取一个名字到预备链表上
-
-		head = add_name_to_the_list(head, get_new_name);  //把读取的那一个名字放到链表里
-	}
-
-	node_t* temp = head;
-
-	while (NULL != temp)  //打印出来所有的名字
-	{
-		printf("%s    %s    %s\n---------------------------\n", &(temp->Fullname.firstname), temp->Fullname.name, temp->Fullname.fathername);
-		temp = temp->next;
-	}
-
-	printf("Put here your FIO in form: \"firstname name fathername\" \n---------------------------\n(if you want miss some name, you can just push void space)\n");
-	fgets(string, 3 * NAME_LENGTH_MAX, stdin);  //获得搜索关键词
-
-	node_t* zero = (node_t*)malloc(sizeof(node_t));     
-
-	if (NULL == zero)
-	{
-		perror("/nerror NO_006 ");
-		return -1;
-	}
-
-	write_string_to_node_t(string, zero);
-
-	head = compare_the_name (head, zero->Fullname.firstname, zero->Fullname.name, zero->Fullname.fathername);
-
-	while (NULL != head)
-	{
-		printf("%s    %s    %s\n---------------------------\n", &(head->Fullname.firstname), head->Fullname.name, head->Fullname.fathername);
-		head = head->next;
-	}
-
-	delete_linked_list(head);
-	free(zero);
-	free(string);
-	fclose(input);
-
-	return 0;
-
+    char string[MAX_NAME_LENGHT]; // max lenght(firstname, name, fathername) < string
+    node_t* zero = (node_t*)malloc(sizeof(node_t));
+    if (!zero) 
+    {
+        perror("/nerror NO_001 ");
+        exit(1);
+    }
+    zero->next = NULL;
+    node_t* new_node;
+    int key;
+    FILE* input;
+    if (NULL == (input = fopen("TEXT.txt", "rb")))
+    {
+        perror("/nerror NO_002 ");
+    }else
+    {
+        key = fscanf(input, "%s", string);
+        add_name_to_node(zero, string, "N");
+        fscanf(input, "%s", string);
+        add_name_to_node(zero, string, "Fir");
+        fscanf(input, "%s", string);
+        add_name_to_node(zero, string, "Fa");
+        key = fscanf(input, "%s", string);
+        while (key != EOF)
+        {
+            new_node = create_new_node_t();
+            add_name_to_node(new_node, string, "N");
+            fscanf(input, "%s", string);
+            add_name_to_node(new_node, string, "Fir");
+            fscanf(input, "%s", string);
+            add_name_to_node(new_node, string, "Fa");
+            zero = sortNode(zero, new_node);
+            key = fscanf(input, "%s", string);
+        }
+        print_list(zero);
+        compare_the_name(zero);
+        delete_list(zero);
+    }
+    return;
 }
