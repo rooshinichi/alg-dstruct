ec737e96a52c91c014c3600f58f4835c86f99245
rooshinichi <60985778+rooshinichi@users.noreply.github.com>
2021-11-17 21:45:09 +0300

    Update A25.h

--- 4_yuan.g_A25/A25.h
+++ 4_yuan.g_A25/A25.h
@@ -18,8 +18,10 @@ typedef struct node
 node_t* create_new_node_t();
 void print_node(node_t* node);
 void print_list(node_t* zero);
-void add_name_to_node(node_t* node, char* name_part, const char* key_word);
+void add_name_to_node1(node_t* node, char* name_part);
+void add_name_to_node2(node_t* node, char* name_part);
+void add_name_to_node3(node_t* node, char* name_part);
 int insert_the_position(char* node_name, char* nodeInListName, node_t** node, node_t** node_in_list, node_t** head);
 node_t* insert_front_of_the_node(node_t** node, node_t** node_in_list, node_t* head);
-node_t* sortNode(node_t* zero, node_t* node);
+node_t* sort_node(node_t* zero, node_t* node);
 void compare_the_name(node_t* zero);
