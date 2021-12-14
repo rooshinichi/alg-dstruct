c6e64292389939e83e78037362ffd95c7262cb4c
rooshinichi <60985778+rooshinichi@users.noreply.github.com>
2021-12-14 14:17:34 +0300

    Update main.c

--- 4-yuan.g-C2/main.c
+++ 4-yuan.g-C2/main.c
@@ -10,7 +10,9 @@ int num_edges = 0;
 int num_vertex = 0;
 vertex* vertices;
 
-edge_t* create_edge(int start_edge, int end_edge) {
+
+edge_t* create_edge(int start_edge, int end_edge) 
+{
     edge_t* temp = (edge_t*)malloc(sizeof(edge_t));
     if (!temp)
     {
@@ -24,29 +26,35 @@ edge_t* create_edge(int start_edge, int end_edge) {
     return temp;
 }
 
-edge_t* add_edge(edge_t* head, int start_edge, int end_edge) {
-    if (!head) {
+edge_t* add_edge(edge_t* head, int start_edge, int end_edge) 
+{
+    if (!head) 
+    {
         perror("No_002 \n");
         exit(1);
     }
     edge_t* cur_node = head;
     edge_t* temp = (edge_t*)malloc(sizeof(edge_t));
-    if (!temp) {
+    if (!temp) 
+    {
         perror("No_003 \n");
         exit(1);
     }
     temp->next = NULL;
     temp->start_edge = start_edge;
     temp->end_edge = end_edge;
-    while (cur_node->next != NULL) {
+    while (cur_node->next != NULL) 
+    {
         cur_node = cur_node->next;
     }
     cur_node->next = temp;
     return head;
 }
 
-void delete_list(edge_t* head) {
-    while (head != NULL) {
+void delete_list(edge_t* head)
+{
+    while (head != NULL) 
+    {
         edge_t* p;
         p = head;
         head = head->next;
@@ -54,29 +62,39 @@ void delete_list(edge_t* head) {
     }
 }
 
-void func(edge_t* head, int weight) {
+void func(edge_t* head, int weight) 
+{
     edge_t* cur_edge = head;
     fprintf(stdout, "%d ", weight);
     int min_vert_weight = num_vertex;
     vertices[weight].visited = SUCCESS;
     int j = 0;
-    while (1) {
-        if (j == 1) {
+    while (1) 
+    {
+        if (j == 1) 
+        {
             break;
         }
         cur_edge = head;
         min_vert_weight = num_vertex;
-        for (int i = 0; i < num_edges; i++) {
-            if (cur_edge->start_edge == weight) {
-                if (vertices[cur_edge->end_edge].visited == ERROR) {
-                    if (min_vert_weight > cur_edge->end_edge) {
+        for (int i = 0; i < num_edges; i++) 
+        {
+            if (cur_edge->start_edge == weight) 
+            {
+                if (vertices[cur_edge->end_edge].visited == ERROR) 
+                {
+                    if (min_vert_weight > cur_edge->end_edge) 
+                    {
                         min_vert_weight = cur_edge->end_edge;
                     }
                 }
             }
-            if (cur_edge->end_edge == weight) {
-                if (vertices[cur_edge->start_edge].visited == ERROR) {
-                    if (min_vert_weight > cur_edge->start_edge) {
+            if (cur_edge->end_edge == weight) 
+            {
+                if (vertices[cur_edge->start_edge].visited == ERROR) 
+                {
+                    if (min_vert_weight > cur_edge->start_edge) 
+                    {
                         min_vert_weight = cur_edge->start_edge;
                     }
                 }
@@ -84,34 +102,49 @@ void func(edge_t* head, int weight) {
             cur_edge = cur_edge->next;
         }
         j++;
-        if (min_vert_weight != num_vertex) {
+        if (min_vert_weight != num_vertex) 
+        {
             func(head, min_vert_weight);
             j = 0;
         }
     }
 }
 
-int main(void) {
+int main(void) 
+{
     edge_t* head = NULL;
     int start_edge;
     int end_edge;
     scanf("%d", &num_vertex);
     vertices = (vertex*)malloc(num_vertex * sizeof(vertex));
 
-    if (!vertices) {
+    if (!vertices) 
+    {
         perror("No_005 \n");
         exit(1);
     }
-
-    for (int i = 0; i < num_vertex; i++) {
+    for (int i = 0; i < num_vertex; i++) 
+    {
         vertices[i].weight = i;
         vertices[i].visited = ERROR;
     }
-
-    if (fscanf(stdin, "%d %d", &start_edge, &end_edge) != EOF) {
+    if (fscanf(stdin, "%d %d", &start_edge, &end_edge) != EOF)
+    { 
         head = create_edge(start_edge, end_edge);
+        num_edges++;//计数增加
+    }
+    while (fscanf(stdin, "%d %d", &start_edge, &end_edge) != EOF)
+    {
+        head = add_edge(head, start_edge, end_edge);
         num_edges++;
     }
+
+    func(head, 0);
+    delete_list(head);
+    free(vertices);
+
+    return 0;
+}
 
     while (fscanf(stdin, "%d %d", &start_edge, &end_edge) != EOF) {
         head = add_edge(head, start_edge, end_edge);
